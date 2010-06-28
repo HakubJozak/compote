dbd7754acd10b71b2aaeb9d21baaa8ca9dfa0e2c
Jakub Hozak <jakub@klaipeda.(none)>
2010-06-28 10:36:05 +0200

    destination not working...yet

--- lib/compote.rb
+++ lib/compote.rb
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
+
 class Compote < Thor::Group
   include Thor::Actions
 
   desc "Generates fresh new game."
 
   argument :game, :type => :string, :desc => "name of the game", :required => true
-  class_option :git, :default => true, :desc => 'Generate GIT .gitignore file?', :type => :boolean
+
+  class_option :git, :default => true, :desc => 'Generate GIT .gitignore file?',
+                     :type => :boolean, :aliases => '-g'
+
+  class_option :destination, :default => true, :desc => 'Destination directory',
+                             :type => :string, :aliases => '-d'
 
   def self.source_root
     File.join(File.dirname(__FILE__), '/template')
   end
 
-  # def self.destination_root
-  #   File.join(File.dirname(__FILE__), '/game')
-  # end
+  def setup
+    destination_root = File.join(File.dirname(__FILE__), options[:destination]) if options[:destination]
+  end
 
 
   # def create_test_file
@@ -28,6 +34,10 @@ class Compote < Thor::Group
       empty_directory("#{game}/#{dir}")      
     end
   end
+
+  def create_game
+    template 'app.rb.erb', "#{game}/lib/#{game}.rb"
+  end
   
   def create_optionals
     copy_file("Gemfile", "#{game}/Gemfile") if options[:bundler]
@@ -41,3 +51,4 @@ class Compote < Thor::Group
   end
 
 end
+
